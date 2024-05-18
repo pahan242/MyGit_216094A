ff6847dcd0e3ca1ec5521bbd632a9750eb346efd
sasi1219 <sasinidr@gmail.com>
2024-05-19 00:04:01 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,5 @@
 from van import Van
+from bike import Bike
 
 print("Pahan")
 
@@ -8,4 +9,7 @@ print(value)
 van1 = Van("A")
 van2 = Van("B")
 
+bike1 = Bike("C", "Yamaha")
+
 print(van2.name)
+print(bike1.type)
